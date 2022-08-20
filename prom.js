54dd9292ac5d4acb2daca689f19a160cf85f118c
arun <arunmadambathl@gmail.com>
2022-08-20 12:59:34 +0530

    20/08/2022

---
+++ prom.js
@@ -0,0 +1,11 @@
+var taskPromise=new Promise((res,rej)=>{
+    let iscomplted=true;
+    if(iscomplted){
+        Res ("task completed")
+
+    }
+})
+
+
+
+taskPromise.then(msg=>console.log("lets start task2")).catch(er=>console.log("error"))
